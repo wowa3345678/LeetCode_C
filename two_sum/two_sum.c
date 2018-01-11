2747492e13bc2203fff3e75e2492ac3b7b7d97f2
RudyLiu <semi-smile@hotmail.com>
2018-01-11 16:40:42 +0800

    Update two_sum.c

--- two_sum/two_sum.c
+++ two_sum/two_sum.c
@@ -48,22 +48,3 @@ static int * twosum(int *nums, int numsSize, int target)
     }
     return NULL;
 }
-
-int main(void)
-{
-    //int nums[] = {-1, -2, -3, -4, -5};
-    //int target = -8;
-    //int nums[] = {0,4,3,0};
-    //int target = 0;
-    int nums[] = { 3, 2, 3 };
-    int count = sizeof(nums) / sizeof(*nums);
-    int target = 6;
-    int *indexes = twosum(nums, count, target);
-    if (indexes != NULL) {
-        printf("%d %d\n", indexes[0], indexes[1]);
-    } else {
-        printf("Not found\n");
-    }
-
-    return 0;
-}
