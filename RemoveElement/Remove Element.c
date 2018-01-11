9d2bd5c9961125a7c6b22a0b9ddc57a66a1c8575
RudyLiu <semi-smile@hotmail.com>
2018-01-11 13:46:42 +0800

    27. Remove Element

---
+++ RemoveElement/Remove Element.c
@@ -0,0 +1,14 @@
+int removeElement(int* nums, int numsSize, int val) {
+
+    
+    int count =0;
+    for(int i=0;i<numsSize;i++){
+        if(nums[i]!=val){
+            nums[count] = nums[i];
+            count++;     
+        }
+    }
+    
+    return count;
+        
+}
