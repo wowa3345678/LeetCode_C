23031ce25e6b766dd3ff94aeacbcdfad2913f1a9
RudyLiu <semi-smile@hotmail.com>
2018-01-22 11:18:51 +0800

    Path Sum

---
+++ 112_Path Sum/Path Sum.c
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool hasPathSum(struct TreeNode* root, int sum) {
+    
+    if(root == NULL){
+        return false;
+    } 
+    else if (root->left == NULL && root->right == NULL && root->val == sum){
+        return true;
+    }
+    else{
+        return(hasPathSum(root->left, sum - root->val)||hasPathSum(root->right,sum - root->val));
+    }
+    
+    
+}
