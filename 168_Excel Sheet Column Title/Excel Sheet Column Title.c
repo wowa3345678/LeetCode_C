c6f564d1bf6a7de7ac813813fa35db05b47b378e
RudyLiu <semi-smile@hotmail.com>
2018-01-30 16:17:23 +0800

    Excel Sheet Column Title

---
+++ 168_Excel Sheet Column Title/Excel Sheet Column Title.c
@@ -0,0 +1,24 @@
+char* convertToTitle(int n) {
+    
+    if(n <= 0) return "";
+    
+    char *result = malloc(1024);
+    int len=0;
+    do{
+        result[len++] = (n-1) % 26 +'A';
+        n = (n-1) / 26;
+    }while(n > 0);
+    
+    result[len] = '\0';
+    
+    int i,j;
+    
+    for(i=0,j=len-1;i < j; i++, j--){
+        char c = result[i];
+        result[i] = result[j];
+        result[j] = c;
+    }
+    
+    return result;
+    
+}
